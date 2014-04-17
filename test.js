0a731075e0072ef9e217a58d8ecd10d5805999fb
Kevin Martensson <kevinmartensson@gmail.com>
2014-04-17 12:04:37 +0200

    Add more tests

--- test.js
+++ test.js
@@ -8,6 +8,10 @@ describe('extname()', function () {
     it('should return a files extension and MIME type', function (cb) {
         assert.strictEqual(extname('foobar.tar').ext, 'tar');
         assert.strictEqual(extname('foobar.tar').mime, 'application/x-tar');
+        assert.strictEqual(extname('foobar.py').ext, 'py');
+        assert.strictEqual(extname('foobar.py').mime, 'text/x-script.python');
+        assert.strictEqual(extname('foobar.pnm').ext, 'pnm');
+        assert.strictEqual(extname('foobar.pnm').mime, 'image/x-portable-anymap');
         cb();
     });
 });
