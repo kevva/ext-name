3a535e7cab70345d8df9924b702d5074407b9e7c
Kevin Martensson <kevinmartensson@gmail.com>
2015-01-22 21:02:11 +0100

    Use `ends-with` package

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var endsWith = require('underscore.string').endsWith;
+var endsWith = require('ends-with');
 var extList = require('ext-list');
 var sortKeysLength = require('sort-keys-length');
 
